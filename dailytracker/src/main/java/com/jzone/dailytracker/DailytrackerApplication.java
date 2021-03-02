b6b9504444a087087907b22e697bf280e78cdc9d
Manoj Nilanga <manojnilangadesilva@gmail.com>
2021-03-02 23:58:23 +0530

    initial commit from laptop

---
+++ dailytracker/src/main/java/com/jzone/dailytracker/DailytrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jzone.dailytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailytrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailytrackerApplication.class, args);
+	}
+
+}
---
+++ dailytracker/src/test/java/com/jzone/dailytracker/DailytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzone.dailytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailytrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
